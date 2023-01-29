27771963f97fe551825039dcf32470b67c048c0f
torippy <47879788+torippy1024@users.noreply.github.com>
2023-01-29 13:05:27 +0900

    コンソール出力

--- src/main.py
+++ src/main.py
@@ -4,14 +4,25 @@ import datetime
 from time import sleep
 
 interval = 60
+output_file = "battery.csv"
 
 def task():
-    with open("battery.csv", "a") as f:
+    with open(output_file, "a") as f:
         btr = psutil.sensors_battery()
         print(f"{datetime.datetime.today()},{btr.percent},{btr.power_plugged}", file=f)
+        print(f"{datetime.datetime.today()},{btr.percent},{btr.power_plugged}")
 
-schedule.every(interval).seconds.do(task)
-
-while True:
-    schedule.run_pending()
-    sleep(1)
+if __name__ == "__main__":
+    print("########################################")
+    print("##########   Battery  Watch   ##########")
+    print("########################################")
+    print(f"interval: {interval}[sec]")
+    print(f"output_file: {output_file}")
+    print("Use Ctrl-C to exit")
+    print("")
+    print(f"Executing...")
+    task()
+    schedule.every(interval).seconds.do(task)
+    while True:
+        schedule.run_pending()
+        sleep(1)

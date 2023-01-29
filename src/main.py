8d8b144e9fffd7606dac03f0e3a71ee37de4f273
torippy <47879788+torippy1024@users.noreply.github.com>
2023-01-29 12:53:05 +0900

    intervalを変数に

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ import schedule
 import datetime
 from time import sleep
 
+interval = 60
+
 def task():
     with open("battery.csv", "a") as f:
         btr = psutil.sensors_battery()
         print(f"{datetime.datetime.today()},{btr.percent},{btr.power_plugged}", file=f)
 
-schedule.every(60).seconds.do(task)
+schedule.every(interval).seconds.do(task)
 
 while True:
     schedule.run_pending()
